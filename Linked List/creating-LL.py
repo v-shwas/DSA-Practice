2fde054219c80453c6403cec23a81b9d2a968ff2
Vishwas Patel <vishwaspatelvard@gmail.com>
2026-09-04 20:17:20 +0530

    Create a Linked-List

---
+++ Linked List/creating-LL.py
@@ -0,0 +1,39 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def append(self, data):
+        new_node = Node(data)
+
+        if self.head is None:
+            self.head = new_node
+            return
+
+        current = self.head
+        while current.next:
+            current = current.next
+
+        current.next = new_node
+
+    def display(self):
+        current = self.head
+
+        while current:
+            print(current.data, end=" -> ")
+            current = current.next
+
+        print("None")
+
+ll = LinkedList()
+
+ll.append(10)
+ll.append(20)
+ll.append(30)
+
+ll.display()
